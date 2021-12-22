dbbee9fc7f9bb52ee9e7013874f820333d881a88
Hendrik Roeder <hendrik@hr94.de>
2021-12-22 22:43:35 +0100

    update files

--- syncDotfiles.sh
+++ syncDotfiles.sh
@@ -13,3 +13,9 @@ cp -r ~/.config/gtk-2.0 .config/
 cp -r ~/.config/gtk-3.0 .config/
 cp -r ~/.config/openbox .config/
 cp -r ~/.config/polybar .config/
+cp ~/.config/geany/geany.conf .config/geany
+cp ~/.config/geany/keybindings.conf .config/geany
+cp -r ~/.config/geany/colorschemes .config/geany/
+cp -r ~/.config/geany/filedefs .config/geany/
+cp -r ~/.config/geany/plugins .config/geany/
+cp -r ~/.config/geany/templates .config/geany/
