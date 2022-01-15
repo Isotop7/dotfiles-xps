ae4fc3b4583c227171d2517458aa7bf27a2c0a26
Hendrik Roeder <hendrik@hr94.de>
2022-01-15 17:37:26 +0100

    update dotfiles

--- syncDotfiles.sh
+++ syncDotfiles.sh
@@ -2,6 +2,7 @@
 
 ### home folder
 
+cp ~/.xinitrc .xinitrc
 cp ~/.Xdefaults .Xdefaults
 cp ~/.vimrc .vimrc
 cp ~/.zshrc .zshrc
@@ -11,11 +12,13 @@ cp ~/.alacritty.yml .alacritty.yml
 cp -r ~/.config/colorschemes .config/
 cp -r ~/.config/gtk-2.0 .config/
 cp -r ~/.config/gtk-3.0 .config/
-cp -r ~/.config/openbox .config/
+cp -r ~/.config/xfce4 .config/
+cp -r ~/.config/Thunar .config/
 cp -r ~/.config/polybar .config/
-cp ~/.config/geany/geany.conf .config/geany
-cp ~/.config/geany/keybindings.conf .config/geany
+cp ~/.config/geany/geany.conf .config/geany/
+cp ~/.config/geany/keybindings.conf .config/geany/
 cp -r ~/.config/geany/colorschemes .config/geany/
 cp -r ~/.config/geany/filedefs .config/geany/
 cp -r ~/.config/geany/plugins .config/geany/
 cp -r ~/.config/geany/templates .config/geany/
+cp -r ~/.config/btop .config/
