de87af1dd3ee9c93274e05e90a1a5d0049013617
Hendrik Roeder <hendrik@hr94.de>
2022-01-15 17:45:23 +0100

    update dotfiles

--- syncDotfiles.sh
+++ syncDotfiles.sh
@@ -15,6 +15,7 @@ cp -r ~/.config/gtk-3.0 .config/
 cp -r ~/.config/xfce4 .config/
 cp -r ~/.config/Thunar .config/
 cp -r ~/.config/polybar .config/
+cp -r ~/.config/autostart .config/
 cp ~/.config/geany/geany.conf .config/geany/
 cp ~/.config/geany/keybindings.conf .config/geany/
 cp -r ~/.config/geany/colorschemes .config/geany/
